4a45bf64a7cb8c224e8f485934af5acbb5cfae32
Ivan Koop <ivan@clearsumm.it>
2021-07-31 12:46:13 -0400

    fix allowed hosts

--- iotd/settings.py
+++ iotd/settings.py
@@ -32,6 +32,17 @@ ALLOWED_HOSTS = [
     # 'your_beanstalk_cname_here'
 ]
 
+import requests
+try:
+    internal_ip = requests.get('http://instance-data/latest/meta-data/local-ipv4').text
+except requests.exceptions.ConnectionError:
+    pass
+else:
+    ALLOWED_HOSTS.append(internal_ip)
+del requests
+
+
+print("ALLOWED_HOSTS", ALLOWED_HOSTS)
 
 # Application definition
 
