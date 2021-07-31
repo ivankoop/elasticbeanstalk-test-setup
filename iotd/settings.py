36db9016c2a8a9eef245b1411dde366761d38fa6
Ivan Koop <ivan@clearsumm.it>
2021-07-31 15:29:52 -0400

    fix redis config

--- iotd/settings.py
+++ iotd/settings.py
@@ -134,7 +134,7 @@ if 'AWS_ACCESS_KEY_ID' in os.environ:
     AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
 # Celery Configuration Options
-BROKER_URL = "redis://aws-my-1npgbj51ycz7h.ponlyf.0001.usw2.cache.amazonaws.com:6379/0"
+BROKER_URL = f"redis://{os.environ['REDIS_CLUSTER_ADDRESS']}:6379/0"
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
